6a4aad374c160fdcbc24175caad06b3a0155130d
Venkatesh Thommandru <venkatesh.thomm@gmail.com>
2026-02-21 14:41:05 +0530

    bucket name added

--- terraform/ec2/data.tf
+++ terraform/ec2/data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "k8s-devsecops-tfstate-bucket"
+    bucket = "venkatesh-devsecops-tfstate-bucket"
     key    = "vpc/terraform.tfstate"
     region = "us-east-1"
   }
