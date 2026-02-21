2cd75bc0156e2d7820d2dee629c7595174ca18a1
Venkatesh Thommandru <venkatesh.thomm@gmail.com>
2026-02-21 14:27:06 +0530

    lock file added

--- terraform/vpc/backend.tf
+++ terraform/vpc/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "k8s-devsecops-tfstate-bucket"
-    key            = "vpc/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "k8s-devsecops-tfstate-lock"
-    encrypt        = true
+    bucket       = "k8s-devsecops-tfstate-bucket"
+    key          = "vpc/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
