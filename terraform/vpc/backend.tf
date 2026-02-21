189f83c860b17c9dd4b22d9dd1c7338be12aa1ed
Venkatesh Thommandru <venkatesh.thomm@gmail.com>
2026-02-21 14:36:30 +0530

    bucket name added

--- terraform/vpc/backend.tf
+++ terraform/vpc/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "k8s-devsecops-tfstate-bucket"
+    bucket       = "venkatesh-devsecops-tfstate-bucket"
     key          = "vpc/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
